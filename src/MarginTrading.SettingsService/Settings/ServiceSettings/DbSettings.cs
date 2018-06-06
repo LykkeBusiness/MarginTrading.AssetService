a64bc055675216f62d6610af433980ca3eb99c99
alpo <alexander.popov@lykke.com>
2018-06-06 12:01:14 +0300

    Revert "Removed Azure checks for emulator"
    This reverts commit 86263e61e4b94aeb873184d6c9a1f4868a68c83b.

--- src/MarginTrading.SettingsService/Settings/ServiceSettings/DbSettings.cs
+++ src/MarginTrading.SettingsService/Settings/ServiceSettings/DbSettings.cs
@@ -4,7 +4,9 @@ namespace MarginTrading.SettingsService.Settings.ServiceSettings
 {
     public class DbSettings
     {
+        [AzureTableCheck]
         public string LogsConnString { get; set; }
+        [AzureTableCheck]
         public string MarginTradingConnString { get; set; }
     }
 }

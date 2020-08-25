134fa92610f9c8d0b4b54a16f32986d98f0fb6d5
Iliyan Iliev <iliyan.iliev@primeholding.com>
2020-08-25 10:25:21 +0300

    LT-2447 undo wrong reshaper renaming

--- src/MarginTrading.AssetService/Controllers/TradingRoutesController.cs
+++ src/MarginTrading.AssetService/Controllers/TradingRoutesController.cs
@@ -165,7 +165,7 @@ namespace MarginTrading.AssetService.Controllers
 
             if (route.Type != null && !Enum.IsDefined(typeof(OrderDirectionContract), route.Type))
             {
-                throw new ArgumentNullException(nameof(route.Type), "Route AssetType is set to an incorrect value");
+                throw new ArgumentNullException(nameof(route.Type), "Route Type is set to an incorrect value");
             }
 
             if (!string.IsNullOrEmpty(route.TradingConditionId)

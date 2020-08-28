837ada56360303edbe97508d02fbcfc3c7d16ee4
Gennadiy Ponomarev <ponomarev@tochka.com>
2020-08-28 11:40:14 +0500

    LT-2471: Sync contract's MaxLength with db MaxLength

--- src/MarginTrading.AssetService.Contracts/Products/AddProductRequest.cs
+++ src/MarginTrading.AssetService.Contracts/Products/AddProductRequest.cs
@@ -1,26 +1,37 @@
+using System.ComponentModel.DataAnnotations;
+
 namespace MarginTrading.AssetService.Contracts.Products
 {
     public class AddProductRequest
     {
         // primary id
+        [MaxLength(400)]
         public string ProductId { get; set; }
         
+        [MaxLength(400)]
         public string AssetType { get; set; }
         
+        [MaxLength(400)]
         public string Category { get; set; }
         
+        [MaxLength(400)]
         public string Comments { get; set; }
         
         public int ContractSize { get; set; }
 
+        [MaxLength(400)]
         public string IsinLong { get; set; }
         
+        [MaxLength(400)]
         public string IsinShort { get; set; }
         
+        [MaxLength(400)]
         public string Issuer { get; set; }
         
+        [MaxLength(400)]
         public string Market { get; set; }
         
+        [MaxLength(400)]
         public string MarketMakerAssetAccountId { get; set; }
         
         public int MaxOrderSize { get; set; }
@@ -33,25 +44,34 @@ namespace MarginTrading.AssetService.Contracts.Products
         
         public decimal MinOrderEntryInterval { get; set; }
 
+        [MaxLength(400)]
         public string Name { get; set; }
         
+        [MaxLength(400)]
         public string NewsId { get; set; }
         
+        [MaxLength(400)]
         public string Keywords { get; set; }
 
+        [MaxLength(400)]
         public string PublicationRic { get; set; }
 
+        [MaxLength(400)]
         public string SettlementCurrency { get; set; }
 
         public bool ShortPosition { get; set; }
 
+        [MaxLength(400)]
         public string Tags { get; set; }
 
+        [MaxLength(400)]
         public string TickFormula { get; set; }
 
         // underlying primary id
+        [MaxLength(400)]
         public string UnderlyingMdsCode { get; set; }
 
+        [MaxLength(400)]
         public string ForceId { get; set; }
 
         public int Parity { get; set; }

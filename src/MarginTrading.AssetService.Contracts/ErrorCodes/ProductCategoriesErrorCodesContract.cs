2680d5e64e8c6f66f5c1ae3167e5dca7db0e4397
Gennady Ponomarev <gennady.ponomarev@lykke-business.ch>
2020-09-11 17:59:21 +0500

    LT-2535: Add CategoryStringIsNotValid error core

--- src/MarginTrading.AssetService.Contracts/ErrorCodes/ProductCategoriesErrorCodesContract.cs
+++ src/MarginTrading.AssetService.Contracts/ErrorCodes/ProductCategoriesErrorCodesContract.cs
@@ -8,5 +8,6 @@ namespace MarginTrading.AssetService.Contracts.ErrorCodes
         CannotDeleteNonLeafCategory,
         ParentHasAttachedProducts,
         CannotDeleteCategoryWithAttachedProducts,
+        CategoryStringIsNotValid,
     }
 }

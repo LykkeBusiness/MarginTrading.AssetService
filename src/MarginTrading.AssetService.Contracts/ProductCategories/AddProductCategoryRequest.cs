5c55eb293c3d44893876c3a76a8a823fbd1d880f
Gennady Ponomarev <gennady.ponomarev@lykke-business.ch>
2020-09-15 13:36:14 +0500

    LT-2556: Make category required in AddProductCategoryRequest

--- src/MarginTrading.AssetService.Contracts/ProductCategories/AddProductCategoryRequest.cs
+++ src/MarginTrading.AssetService.Contracts/ProductCategories/AddProductCategoryRequest.cs
@@ -1,9 +1,11 @@
+using System.ComponentModel.DataAnnotations;
 using MarginTrading.AssetService.Contracts.Core;
 
 namespace MarginTrading.AssetService.Contracts.ProductCategories
 {
     public class AddProductCategoryRequest : UserRequest
     {
+        [Required]
         public string Category { get; set; }
     }
 }

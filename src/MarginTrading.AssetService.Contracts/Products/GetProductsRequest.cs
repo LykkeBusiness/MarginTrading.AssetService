79cf55e1ec10d293706d21de99810139c646265d
Iliyan Iliev <iliyan.iliev@primeholding.com>
2020-10-05 18:05:07 +0300

    LT-2590 Add query attribute

--- src/MarginTrading.AssetService.Contracts/Products/GetProductsRequest.cs
+++ src/MarginTrading.AssetService.Contracts/Products/GetProductsRequest.cs
@@ -1,9 +1,13 @@
-﻿namespace MarginTrading.AssetService.Contracts.Products
+﻿using Refit;
+
+namespace MarginTrading.AssetService.Contracts.Products
 {
     public class GetProductsRequest
     {
+        [Query(CollectionFormat.Multi)]
         public string[] MdsCodes { get; set; }
 
+        [Query(CollectionFormat.Multi)]
         public string[] ProductIds { get; set; }
     }
 }

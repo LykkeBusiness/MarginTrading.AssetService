d880ebc05be01228499a6e759cf41669f277403b
Iliyan Iliev <iliyan.iliev@primeholding.com>
2020-10-05 18:35:51 +0300

    LT-2590 remove query attributes

--- src/MarginTrading.AssetService.Contracts/Products/GetProductsRequest.cs
+++ src/MarginTrading.AssetService.Contracts/Products/GetProductsRequest.cs
@@ -1,13 +1,9 @@
-﻿using Refit;
-
-namespace MarginTrading.AssetService.Contracts.Products
+﻿namespace MarginTrading.AssetService.Contracts.Products
 {
     public class GetProductsRequest
     {
-        [Query(CollectionFormat.Multi)]
         public string[] MdsCodes { get; set; }
 
-        [Query(CollectionFormat.Multi)]
         public string[] ProductIds { get; set; }
     }
 }

2eb4d3afe3096c165aa6cae830a97b7219b35ebd
Andrey Volkov <avolkov.dev.10x@gmail.com>
2020-12-17 12:24:57 +0300

    fix Pr notes: update the cache only if the dividend fields were changed.

--- src/MarginTrading.AssetService.Services/RabbitMq/Handlers/BrokerSettingsChangedHandler.cs
+++ src/MarginTrading.AssetService.Services/RabbitMq/Handlers/BrokerSettingsChangedHandler.cs
@@ -1,5 +1,4 @@
-﻿using System;
-using System.Threading.Tasks;
+﻿using System.Threading.Tasks;
 using Common.Log;
 using Lykke.Common.Log;
 using Lykke.Snow.Mdm.Contracts.Models.Events;
@@ -21,8 +20,25 @@ namespace MarginTrading.AssetService.Services.RabbitMq.Handlers
 
         public async Task Handle(BrokerSettingsChangedEvent e)
         {
-            _log.Info("Invalidating cache for all legacy assets");
-            await _legacyAssetsCacheUpdater.UpdateAll(e.Timestamp);
+            if (NeedToInvalidateCache(e))
+            {
+                _log.Info("Invalidating cache for all legacy assets", context:e);
+                await _legacyAssetsCacheUpdater.UpdateAll(e.Timestamp);
+
+            }
+            else
+            {
+                _log.Info("Cache invalidation not required", context: e);
+            }
+        }
+
+        private static bool NeedToInvalidateCache(BrokerSettingsChangedEvent e)
+        {
+            //invalidating cache only if fields related to assets changed
+            //see CronutAssetExtensions.SetDividendFactorFields for details
+            return !e.NewValue.Dividends871MPercent.Equals(e.OldValue.Dividends871MPercent)
+                   || !e.NewValue.DividendsLongPercent.Equals(e.OldValue.DividendsLongPercent)
+                   || !e.NewValue.DividendsShortPercent.Equals(e.OldValue.DividendsShortPercent);
         }
     }
 }

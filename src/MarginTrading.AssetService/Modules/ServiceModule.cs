18026f0a29338ba30273573bf92937e22461a2af
Andrei Tarutin <tarutin_andrew@mail.ru>
2021-01-15 17:03:09 +0300

    fixed asset types with 0 interest rate injection

--- src/MarginTrading.AssetService/Modules/ServiceModule.cs
+++ src/MarginTrading.AssetService/Modules/ServiceModule.cs
@@ -177,7 +177,7 @@ namespace MarginTrading.AssetService.Modules
             builder.RegisterType<LegacyAssetsService>()
                 .As<ILegacyAssetsService>()
                 .WithParameter(TypedParameter.From(_settings.CurrentValue.BrokerId))
-                .WithParameter(TypedParameter.From(_settings.CurrentValue.AssetTypesWithZeroInterestRates))
+                .WithParameter(new NamedParameter("assetTypesWithZeroInterestRate", _settings.CurrentValue.AssetTypesWithZeroInterestRates))
                 .SingleInstance();
 
             builder.RegisterType<LegacyAssetCache>()
@@ -186,7 +186,7 @@ namespace MarginTrading.AssetService.Modules
 
             builder.RegisterType<LegacyAssetsCacheUpdater>()
                 .As<ILegacyAssetsCacheUpdater>()
-                .WithParameter(TypedParameter.From(_settings.CurrentValue.AssetTypesWithZeroInterestRates))
+                .WithParameter(new NamedParameter("assetTypesWithZeroInterestRate", _settings.CurrentValue.AssetTypesWithZeroInterestRates))
                 .SingleInstance();
 
             builder.RegisterType<UnderlyingChangedHandler>()

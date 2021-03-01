3c2d015b08d82e855e889ddb293388de1e0f507f
Andrei Tarutin <tarutin_andrew@mail.ru>
2021-03-02 00:44:08 +0300

    Added cache to the contract package to be reused across multiple services

---
+++ src/MarginTrading.AssetService.Contracts/DependencyInjectionExtensions.cs
@@ -0,0 +1,18 @@
+using MarginTrading.AssetService.Contracts.ClientProfileSettings;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace MarginTrading.AssetService.Contracts
+{
+    public static class DependencyInjectionExtensions
+    {
+        /// <summary>
+        /// Registers <see cref="IClientProfileSettingsCache"/> in the .NET Core DI Container.
+        /// Requires <see cref="IClientProfileSettingsApi"/> to be registered.
+        /// </summary>
+        /// <param name="services"></param>
+        public static void AddClientProfileSettingsCache(this IServiceCollection services)
+        {
+            services.AddSingleton<IClientProfileSettingsCache, ClientProfileSettingsCache>();
+        }
+    }
+}

40568fcc3f61fff392c786b38fb1d7cf1ee761f4
Andrei Tarutin <tarutin_andrew@mail.ru>
2021-03-02 10:45:55 +0300

    added IClientProfileCache.Start

--- src/MarginTrading.AssetService.Contracts/ClientProfiles/IClientProfileCache.cs
+++ src/MarginTrading.AssetService.Contracts/ClientProfiles/IClientProfileCache.cs
@@ -2,6 +2,7 @@ namespace MarginTrading.AssetService.Contracts.ClientProfiles
 {
     public interface IClientProfileCache
     {
+        void Start();
         void AddOrUpdate(ClientProfileContract clientProfile);
         void Remove(ClientProfileContract clientProfile);
     }

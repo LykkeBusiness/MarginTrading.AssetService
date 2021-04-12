b2b2bd0b58ffdddf187035fba26480bf34fdfbb4
Andrei Tarutin <tarutin_andrew@mail.ru>
2021-04-12 11:44:36 +0300

    Updated IClientProfileSettingsCache, added TryGetValue method

--- src/MarginTrading.AssetService.Contracts/ClientProfileSettings/IClientProfileSettingsCache.cs
+++ src/MarginTrading.AssetService.Contracts/ClientProfileSettings/IClientProfileSettingsCache.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections.Generic;
 
 namespace MarginTrading.AssetService.Contracts.ClientProfileSettings
@@ -5,9 +6,15 @@ namespace MarginTrading.AssetService.Contracts.ClientProfileSettings
     public interface IClientProfileSettingsCache
     {
         void Start();
+        
         void AddOrUpdate(ClientProfileSettingsContract clientProfileSettings);
+        
         void Remove(ClientProfileSettingsContract clientProfileSettings);
+        
+        [Obsolete("Will be removed in future releases. Please use TryGetValue instead.")]
         ClientProfileSettingsContract GetByIds(string profileId, string assetType);
+
+        bool TryGetValue(string profileId, string assetType, out ClientProfileSettingsContract result);
 
         IReadOnlyList<ClientProfileSettingsContract> GetByAssetType(string assetType, bool availableOnly = false);
     }

40b7ccf667569e60a98c6bcd3654a4fc83179694
Aliaksandr Vashetsin <aliaksandr.vashetsin@lykke-business.ch>
2021-09-14 12:53:13 +0200

    LT-3491: fix decimal range attributes

--- src/MarginTrading.AssetService.Contracts/Products/AddProductRequest.cs
+++ src/MarginTrading.AssetService.Contracts/Products/AddProductRequest.cs
@@ -107,13 +107,13 @@ namespace MarginTrading.AssetService.Contracts.Products
         
         public DateTime? StartDate { get; set; }
 
-        [Range(0, 200)]
+        [Range(0.0, 200.0)]
         public decimal? DividendsLong { get; set; }
 
-        [Range(0, 200)]
+        [Range(0.0, 200.0)]
         public decimal? DividendsShort { get; set; }
 
-        [Range(0, 100)]
+        [Range(0.0, 100.0)]
         public decimal? Dividends871M { get; set; }
 
         [Required]

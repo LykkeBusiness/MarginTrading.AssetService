6a77a37295897390031888e855f716a3f681b169
Alexey Sobinov <alexey.sobinov@gmail.com>
2022-04-29 01:25:33 +0500

    dotcover added

--- src/MarginTrading.AssetService/entrypoint.sh
+++ src/MarginTrading.AssetService/entrypoint.sh
@@ -1,6 +1,5 @@
 #!/bin/bash -l
 set -eu
 
-cd /app
 ./dotCover.sh cover --output="/app/logs/assetservice/test.html" --reportType="HTML" --targetExecutable="/usr/share/dotnet/dotnet" --targetArguments="./MarginTrading.AssetService.dll" --startInstance="1"
 ./dotCover.sh send --Instance="1" --Command="Cover"

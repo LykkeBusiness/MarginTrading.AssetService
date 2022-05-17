b8848593bb4f6f244eb8041fff40fb1b38e620f8
Alexey Sobinov <38539246+alexey-sobinov@users.noreply.github.com>
2022-05-17 18:42:56 +0500

    Update entrypoint.sh

--- src/MarginTrading.AssetService/entrypoint.sh
+++ src/MarginTrading.AssetService/entrypoint.sh
@@ -4,3 +4,4 @@ printenv
 
 /app/dotCover.sh cover --output="/app/logs/assetservice/test.html" --reportType="HTML" --targetExecutable="/usr/share/dotnet/dotnet" --targetArguments="/app/MarginTrading.AssetService.dll" --startInstance="1"
 /app/dotCover.sh send --Instance="1" --Command="Cover"
+sleep infinity

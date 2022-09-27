a28e95a9ca402ff58baf97a592b2dd87a6338360
atarutin <andrey.tarutin@lykke-business.ch>
2022-09-27 12:19:57 +0300

    fix(LT-4152): another mapping fixes

---
+++ src/MarginTrading.AssetService.Services/Mapping/MarketProfile.cs
@@ -0,0 +1,30 @@
+using AutoMapper;
+
+using Lykke.Snow.Common.WorkingDays;
+
+using MarginTrading.AssetService.Contracts.Market;
+using MarginTrading.AssetService.Contracts.MarketSettings;
+using MarginTrading.AssetService.Core.Domain;
+
+namespace MarginTrading.AssetService.Services.Mapping
+{
+    public class MarketProfile : Profile
+    {
+        public MarketProfile()
+        {
+            CreateMap<MarketSchedule, MarketScheduleContract>();
+            CreateMap<MarketSettings, MarketSettingsContract>()
+                .ForMember(dest => dest.Open, opt => opt.MapFrom(x => x.MarketSchedule.Open))
+                .ForMember(dest => dest.Close, opt => opt.MapFrom(x => x.MarketSchedule.Close))
+                .ForMember(dest => dest.Timezone, opt => opt.MapFrom(x => x.MarketSchedule.TimeZoneId));
+            CreateMap<MarketSettingsContract, MarketSettings>()
+                .ForMember(dest => dest.MarketSchedule, opt => opt.MapFrom<MarketScheduleResolver>());
+
+            CreateMap<AddMarketSettingsRequest, MarketSettingsCreateOrUpdateDto>();
+            CreateMap<UpdateMarketSettingsRequest, MarketSettingsCreateOrUpdateDto>()
+                .ForMember(x => x.Id, opt => opt.Ignore());
+
+            CreateMap<Market, MarketContract>();
+        }
+    }
+}

764f16c1a8ca406d5a20fd09f20aaf345c5f3f9f
Michael Kobzev <69039160+kobzev@users.noreply.github.com>
2025-02-14 08:19:39 +0300

    adjust logs

--- src/MarginTrading.AssetService/Program.cs
+++ src/MarginTrading.AssetService/Program.cs
@@ -1,10 +1,6 @@
 ﻿// Copyright (c) 2019 Lykke Corp.
 // See the LICENSE file in the project root for more information.
 
-using System.Threading.Tasks;
-
-using Lykke.Snow.Common.Startup;
-
 using MarginTrading.AssetService.Startup;
 using Microsoft.AspNetCore.Builder;
 
@@ -16,7 +12,7 @@ namespace MarginTrading.AssetService
 
         public static async Task Main(string[] args)
         {
-            await StartupWrapper.StartAsync(async () =>
+            await new StartupWrapper().StartAsync(async () =>
             {
                 var builder = WebApplication.CreateBuilder(args);
 
